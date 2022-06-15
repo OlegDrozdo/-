70f4c277a9498f0b5cfb587ad3a9160ac88cd2c2
OlegDrozdov <dr-oleg-103@bk.ru>
2022-06-16 00:18:47 +0300

    Scanner branch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int num = 12345678;
-        System.out.printf(Locale.US, "%,d", num);
+        Scanner num = new Scanner(System.in);
+        int num1 = 12345678;
+        System.out.printf(Locale.US, "%,d", num1);
     }
 }
